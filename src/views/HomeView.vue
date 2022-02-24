2fad19e097c7a75fe103ea09c8ef9db4b5c7ed80
Dennis Gauss <chan05@hotmail.de>
2022-02-24 16:03:25 +0100

    added button and input field

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,5 +4,30 @@
 <template>
   <main>
     <div> <p> <h1>Scrum Planning Poker</h1> </p> </div>
+    <button class="btn btn-indigo outline" @click="createRoom">Create Room
+    </button>
+    <br>
+    <br>
+    <button class="btn btn-indigo outline" @click="joinRoom">Join Room
+    </button>
+    <input v-model="message" placeholder="Enter Room ID">
   </main>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+         methods: {
+           createRoom(){
+              alert('Created');
+           },
+           joinRoom(){
+             alert('Joined');
+           } 
+         }
+    }
+</script>
